dcac00afd13357505f658c0275b0445743cd6e69
Rohan M. <rohanmishra2017@gmail.com>
2014-10-12 08:11:18 -0500

    wow
    arthur

---
+++ signuppage.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<!-- Favicon -->
+		<link rel="icon" type="image/png" href="../images/favicon.ico">	
+
+		<!-- Latest compiled and minified Bootstrap CSS -->		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+		<!-- Landing.css import -->
+		<link rel="stylesheet" type="text/css" href="css/signuppage.css">
+
+		<!-- Animate.css -->
+		<link rel="stylesheet" type="text/css" href="css/animate.css">
+
+	<head>
+	<body>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h3> Sign In/Sign Up </h3>
+				</div>
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-2">
+					<p> <button type="button" class="btn btn-default btn-lg">
+					  Back to Lance.Me
+					</button></p>
+				</div>
+				<div class="col-md-4">
+					<div class="panel panel-default">
+					  <div class="panel-heading">
+					    <h3 class="panel-title"> Sign In </h3>
+					  </div>
+					  <div class="panel-body">
+					    <div class="input-group">
+						  <input type="text" class="form-control" placeholder="Username">
+						</div>
+						<div id="password" class="input-group">
+						  <input type="text" class="form-control" placeholder="Password">
+						</div>
+						<button type="button" class="btn btn-default">
+						  Sign In
+						</button>
+					  </div>
+					</div>
+				</div>	
+				<div class="col-md-6">
+					<div class="panel panel-default">
+					  <div class="panel-heading">
+					    <h3 class="panel-title"> Sign Up </h3>
+					  </div>
+					  <div class="panel-body">
+					  	<h5> Name </h5>
+					    <div class="input-group">
+						  <input type="text" class="form-control" placeholder="Johnson">
+						</div>
+						<div id="password" class="input-group">
+						  <input type="text" class="form-control" placeholder="Lin">
+						</div>
+						<h5> Email </h5>
+						<div id="password" class="input-group">
+						  <input type="text" class="form-control" placeholder="johnson.lin@example.com">
+						</div>
+						<h5> Password </h5>
+						<div id="password" class="input-group">
+						  <input type="text" class="form-control" placeholder="At least eight characters">
+						</div>
+						<h5> Confirm Password </h5>
+						<div id="password" class="input-group">
+						  <input type="text" class="form-control" placeholder="Same as above">
+						</div>
+						<button type="button" class="btn btn-default">
+						  Sign Up
+						</button>
+						</div>
+					  </div>
+					</div>
+				</div>	
+			</div>
+		</div>
+
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/main.js"></script>
+
+	   	<!-- jQuery Version 1.11.0 -->
+	    <script src="js/jquery-1.11.0.js"></script>
+
+		<!-- Scrolling Nav JavaScript -->
+		<script src="js/jquery.easing.min.js"></script>
+		<script src="js/scrolling-nav.js"></script>
+		
+		<!-- Bootstrap Minified JS -->
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+<html>
+
+<!-- #weneedarthur -->
